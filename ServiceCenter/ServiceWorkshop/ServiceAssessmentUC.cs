349038fb112141d316774d623f225939ffc0f227
F1n5a <finsakusumaputra@gmail.com>
2026-01-13 09:12:50 +0900

    Little Update Show Availabel Sparepart in dgvSparepart

--- ServiceCenter/ServiceWorkshop/ServiceAssessmentUC.cs
+++ ServiceCenter/ServiceWorkshop/ServiceAssessmentUC.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServiceCenter.core.network;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,13 +17,53 @@ namespace ServiceCenter.ServiceWorkshop
         public ServiceAssessmentUC(int serviceID)
         {
             InitializeComponent();
+            loadSparepart();
             serviceId = serviceID;
-            MessageBox.Show(serviceId.ToString());
         }
 
         private void dgvSparePart_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            if(e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvSparePart.Rows[e.RowIndex];
+                if (dgvSparePart.Columns[e.ColumnIndex].Name == "btnAction")
+                {
+                    string query = "UPDATE spareparts SET stock = stock - 1 WHERE sparepart_id = @sid";
+                    int i = DBHelper.executeNonQuery(query,
+                        new Microsoft.Data.SqlClient.SqlParameter("@sid", Convert.ToInt32(row.Cells["sparepart_id"].Value))
+                    );
+                    if(i > 0)
+                    {
+                        loadSparepart();
+                    }
+                }
+            }
+        }
 
+        private void addButtonUse()
+        {
+            if (!dgvSparePart.Columns.Contains("btnAction"))
+            {
+                DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+                btn.Text = "Use";
+                btn.HeaderText = "Action";
+                btn.Name = "btnAction";
+                btn.UseColumnTextForButtonValue = true;
+                dgvSparePart.Columns.Add(btn);
+            }
+        }
+        private void loadSparepart()
+        {
+            string query = @"
+                    SELECT 
+                        sparepart_id,
+                        sparepart_code AS [Sparepart Code], 
+                        sparepart_name AS [Sparepart],
+                        stock AS Stock
+                    FROM spareparts 
+                    WHERE stock > 0";
+            dgvSparePart.DataSource = DBHelper.executeQuery(query);
+            addButtonUse();
         }
     }
 }
